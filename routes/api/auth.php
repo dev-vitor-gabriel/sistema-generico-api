b3d33b7d8de99f7b88c54697b259a1f772375e9a
Fernando <fpinheiro@nwsoft.com.br>
2023-08-29 22:03:53 -0300

    feat: separar rotas em arquivos

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -14,10 +14,10 @@ use App\Http\Controllers\API\AuthController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::middleware('api')
-->prefix('auth')
-->group(base_path('routes/api/auth.php'));
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
 });
