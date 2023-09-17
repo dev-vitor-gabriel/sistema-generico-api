8df586c4dc542f12da3de45e994820d29f4ab093
dev-vitor-gabriel <dev.vitor.gabriel@gmail.com>
2023-09-17 03:04:49 -0300

    feat: criação de Model de Cliente;

---
+++ app/Models/Cliente.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    use HasFactory;
+     
+    protected $table = "tb_cliente";
+
+    protected $fillable = [
+        'des_cliente_cli',
+        'telefone_cliente_cli',
+        'documento_cliente_cli',
+        'endereco_cliente_cli',
+        'is_ativo_cli'
+    ];
+
+    public static function getAll() {
+        $data = Cliente::select(['*'])->get();
+        return response()->json($data);
+    }
+
+    public static function getById(Int $id = null) {    
+        if($id) {
+            $data = Cliente::select(['*'])->where('id_cliente_cli', $id)->get();
+        }else{
+            $data = Cliente::select(['*'])->get();
+        }
+        return response()->json($data);
+    }
+
+    public static function updateReg(Int $id_cliente, $obj) {
+        Cliente::where('id_cliente_cli', $id_cliente)
+        ->update([
+            'des_material_mte' => $obj->des_material_mte
+        ]);
+    }
+
+    public static function deleteReg($id_cliente) {
+        Cliente::where('id_cliente_cli', $id_cliente)
+        ->update([
+            'is_ativo_cli' => 0
+        ]);
+    }
+
+}
