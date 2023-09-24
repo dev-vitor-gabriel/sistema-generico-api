25dafbf7a3bf75b914c5c357310f8641bf290343
dev-vitor-gabriel <dev.vitor.gabriel@gmail.com>
2023-09-24 03:08:46 -0300

    fix: ajuste na migration;

--- database/migrations/2023_08_29_233742_create_materials_table.php
+++ database/migrations/2023_08_29_233742_create_materials_table.php
@@ -13,8 +13,11 @@ return new class extends Migration
     {
         Schema::create('tb_material', function (Blueprint $table) {
             $table->id('id_material_mte');
+            $table->unsignedBigInteger('id_unidade_mte');
             $table->string('des_material_mte', 255);
+            $table->float('vlr_material_mte');
             $table->boolean('is_ativo_mte');
+            $table->foreign('id_unidade_mte')->references('id_unidade_und')->on('tb_unidade');
             $table->timestamps();
         });
     }
