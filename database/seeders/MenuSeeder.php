4debc21faa7b5797095b8dbe730cb81a1b13c456
dev-vitor-gabriel <dev.vitor.gabriel@gmail.com>
2023-09-27 00:04:38 -0300

    feat: criando o menu lateral;

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -15,52 +15,171 @@ class MenuSeeder extends Seeder
     {
         DB::table('tb_menu')->insert([
             'id_father_mnu' => null,
-            'des_menu_mnu' => 'Home',
-            'icon_menu_mnu' => 'icon_home.png',
-            'path_menu_mnu' => '',
-            'num_ordem_mnu' => 0,
-        ]);
-        DB::table('tb_menu')->insert([
-            'id_father_mnu' => null,
-            'des_menu_mnu' => 'Serviço',
-            'icon_menu_mnu' => 'servico.png',
+            'des_menu_mnu'  => 'Home',
+            'icon_menu_mnu' => 'ImHome',
             'path_menu_mnu' => '',
             'num_ordem_mnu' => 1,
         ]);
         DB::table('tb_menu')->insert([
-            'id_father_mnu' => 2,
-            'des_menu_mnu' => 'Novo',
-            'icon_menu_mnu' => 'novo_servico.png',
+            'id_father_mnu' => null,
+            'des_menu_mnu' => 'Serviço',
+            'icon_menu_mnu' => 'FaScissors',
             'path_menu_mnu' => '',
             'num_ordem_mnu' => 2,
         ]);
         DB::table('tb_menu')->insert([
             'id_father_mnu' => 2,
-            'des_menu_mnu' => 'Lista',
-            'icon_menu_mnu' => 'lista_servico.png',
+            'des_menu_mnu' => 'Novo Serviço',
+            'icon_menu_mnu' => 'HiMiniPlusCircle',
             'path_menu_mnu' => '',
             'num_ordem_mnu' => 3,
         ]);
         DB::table('tb_menu')->insert([
-            'id_father_mnu' => null,
-            'des_menu_mnu' => 'Almoxarifado',
-            'icon_menu_mnu' => 'almoxarifado_servico.png',
+            'id_father_mnu' => 2,
+            'des_menu_mnu' => 'Finalizar Serviço',
+            'icon_menu_mnu' => 'IoCheckmarkCircle',
             'path_menu_mnu' => '',
             'num_ordem_mnu' => 4,
         ]);
         DB::table('tb_menu')->insert([
-            'id_father_mnu' => 5,
-            'des_menu_mnu' => 'Entrada',
-            'icon_menu_mnu' => 'almoxarifado_servico.png',
+            'id_father_mnu' => 2,
+            'des_menu_mnu' => 'Relatório de Serviço',
+            'icon_menu_mnu' => 'BiSolidReport',
             'path_menu_mnu' => '',
             'num_ordem_mnu' => 5,
         ]);
         DB::table('tb_menu')->insert([
-            'id_father_mnu' => 5,
-            'des_menu_mnu' => 'Movimentações',
-            'icon_menu_mnu' => 'almoxarifado_servico.png',
+            'id_father_mnu' => null,
+            'des_menu_mnu' => 'Almoxarifado',
+            'icon_menu_mnu' => 'AiFillDropboxCircle',
             'path_menu_mnu' => '',
             'num_ordem_mnu' => 6,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 6,
+            'des_menu_mnu' => 'Consulta Estoque',
+            'icon_menu_mnu' => 'HiDocumentSearch',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 7,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 6,
+            'des_menu_mnu' => 'Baixa de Entrada',
+            'icon_menu_mnu' => 'HiArrowDownCircle',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 8,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 6,
+            'des_menu_mnu' => 'Baixa de Saída',
+            'icon_menu_mnu' => 'HiArrowUpCircle',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 9,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 6,
+            'des_menu_mnu' => 'Relatório de Movimentações',
+            'icon_menu_mnu' => 'BiSolidReport',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 10,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => null,
+            'des_menu_mnu' => 'Financeiro',
+            'icon_menu_mnu' => 'RiMoneyDollarCircleFill',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 11,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 11,
+            'des_menu_mnu' => 'A Receber',
+            'icon_menu_mnu' => 'HiArrowDownCircle',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 12,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 11,
+            'des_menu_mnu' => 'A Pagar',
+            'icon_menu_mnu' => 'HiArrowUpCircle',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 13,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 11,
+            'des_menu_mnu' => 'Relatório Financeiro',
+            'icon_menu_mnu' => 'BiSolidReport',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 14,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => null,
+            'des_menu_mnu' => 'Cadastro Base',
+            'icon_menu_mnu' => 'BsWrenchAdjustableCircleFill',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 15,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 15,
+            'des_menu_mnu' => 'Serviço',
+            'icon_menu_mnu' => 'BsWrenchAdjustableCircleFill',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 16,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 16,
+            'des_menu_mnu' => 'Cadastro de Tipo de Serviço',
+            'icon_menu_mnu' => 'HiMiniPlusCircle',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 17,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 15,
+            'des_menu_mnu' => 'Almoxarifado',
+            'icon_menu_mnu' => 'AiFillDropboxCircle',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 18,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 18,
+            'des_menu_mnu' => 'Cadastro de Unidade',
+            'icon_menu_mnu' => 'HiMiniPlusCircle',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 19,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 18,
+            'des_menu_mnu' => 'Cadastro de Materiais',
+            'icon_menu_mnu' => 'HiMiniPlusCircle',
+            'path_menu_mnu' => 'cadastroMaterial',
+            'num_ordem_mnu' => 20,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 18,
+            'des_menu_mnu' => 'Cadastro de Estoque',
+            'icon_menu_mnu' => 'HiMiniPlusCircle',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 21,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 15,
+            'des_menu_mnu' => 'Financeiro',
+            'icon_menu_mnu' => 'RiMoneyDollarCircleFill',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 22,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 22,
+            'des_menu_mnu' => 'Cadastro de método de Pagamento',
+            'icon_menu_mnu' => 'RiCoinsFill',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 23,
+        ]);
+        DB::table('tb_menu')->insert([
+            'id_father_mnu' => 22,
+            'des_menu_mnu' => 'Cadastro de instituições de Pagamento',
+            'icon_menu_mnu' => 'BiSolidBank',
+            'path_menu_mnu' => '',
+            'num_ordem_mnu' => 24,
         ]);
     }
 }
