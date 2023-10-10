1c0f28f31d5c2d0d42b72ba7ceb0436551a6bd3f
dev-vitor-gabriel <dev.vitor.gabriel@gmail.com>
2023-10-09 23:12:50 -0300

    feat: adicionando coluna img;

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            'email' => 'required|string|email',
+            'email'    => 'required|string|email',
             'password' => 'required|string',
         ]);
         $credentials = $request->only('email', 'password');
