ce066ba517a84a62fb1f107e00303b95c41e2e42
dev-vitor-gabriel <dev.vitor.gabriel@gmail.com>
2023-10-09 23:17:40 -0300

    feat: adicionando coluna img;

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -49,7 +49,7 @@ class AuthController extends Controller
             'name'         => 'required|string|max:255',
             'email'        => 'required|string|email|max:255|unique:users',
             'password'     => 'required|string|min:6',
-            'url_img_user' => 'required|string'
+            'url_img_user' => 'string'
         ]);
 
         $user = User::create([
