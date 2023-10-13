8062d1a5f64657cc7cb7ae9fcb626adea3b7685b
dev-vitor-gabriel <dev.vitor.gabriel@gmail.com>
2023-10-13 16:51:36 -0300

    feat: adicionado novas colunas na model;

--- database/migrations/2023_08_29_233949_create_material_movimentacao_items_table.php
+++ database/migrations/2023_08_29_233949_create_material_movimentacao_items_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id('id_movimentacao_item_mit');
             $table->unsignedBigInteger('id_movimentacao_mit');
             $table->unsignedBigInteger('id_material_mit');
-            $table->int('qtd_material_mit');
-            $table->int('vlr_material_mit');
+            $table->integer('qtd_material_mit');
+            $table->integer('vlr_material_mit');
             $table->boolean('is_ativo_mit')->default(true);
             $table->foreign('id_movimentacao_mit')->references('id_movimentacao_mov')->on('tb_material_movimentacao');
             $table->foreign('id_material_mit')->references('id_material_mte')->on('tb_material');
