20c74cc370f5e340cb8fa3eaa1e6401766725f41
dev-vitor-gabriel <dev.vitor.gabriel@gmail.com>
2023-10-13 16:51:27 -0300

    feat: adicionado novas colunas na model;

--- app/Models/MaterialMovimentacaoItem.php
+++ app/Models/MaterialMovimentacaoItem.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class MaterialMovimentacaoItem extends Model
 {
     use HasFactory;
+    protected $table = "tb_material_movimentacao_item";
+
+    protected $fillable = [
+        'id_movimentacao_mit',
+        'id_material_mit',
+        'qtd_material_mit',
+        'vlr_material_mit',
+        'is_ativo_mit'
+    ];
 }
