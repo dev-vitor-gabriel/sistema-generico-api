abfd740116b7e3be2310c5b22f8cdffbfb321f78
dev-vitor-gabriel <dev.vitor.gabriel@gmail.com>
2023-10-21 18:22:48 -0300

    fix: ajuste na ordem das migrations;

--- database/migrations/2023_08_28_234137_create_contas_table.php
+++ database/migrations/2023_08_28_234137_create_contas_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('tb_conta', function (Blueprint $table) {
             $table->id('id_conta_con');
-            $table->integer('id_conta_tipo_con');
             $table->date('dta_conta_con');
             $table->integer('vlr_conta_con');
-            $table->boolean('is_ativo_con')->default(true);
             $table->unsignedBigInteger('id_centro_custo_con');
+            $table->unsignedBigInteger('id_metodo_pagamento_con');
+            $table->unsignedBigInteger('id_conta_tipo_con');
+            $table->boolean('is_ativo_con')->default(true);
             $table->foreign('id_centro_custo_con')->references('id_centro_custo_cco')->on('tb_centro_custo');
+            $table->foreign('id_metodo_pagamento_con')->references('id_metodo_pagamento_tmp')->on('tb_metodo_pagamento');
+            $table->foreign('id_conta_tipo_con')->references('id_conta_tipo_ctp')->on('tb_conta_tipo');
             $table->timestamps();
         });
     }
