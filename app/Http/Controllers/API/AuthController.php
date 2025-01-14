4cbbe3cf725cb6738d1984c6eb237ae3f9a3a94c
“Vitor <dev.vitor.gabriel@gmail.com>
2025-01-14 06:20:22 -0300

    fix: add validacao pra quando a senha for pequena

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -45,11 +45,17 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
+        if(strlen($request->password) <= 6){
+            return response()->json([
+                'message' => 'Your password must be at least 6 characters long.',
+            ], 400);
+        }
+
         $request->validate([
             'name'         => 'required|string|max:255',
             'email'        => 'required|string|email|max:255|unique:users',
             'password'     => 'required|string|min:6',
-            'url_img_user' => 'nullable|file|mimes:jpg,jpeg,png|max:2048', // Validando como arquivo
+            'url_img_user' => 'nullable|file|mimes:jpg,jpeg,png|max:2048',
         ]);
 
         $filePath = null;
