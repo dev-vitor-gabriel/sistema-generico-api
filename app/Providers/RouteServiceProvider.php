b366cb74c2df78f450db74b2469543e13597c192
Jhammes <dean.morais@hotmail.com>
2025-01-27 00:00:51 -0300

    feat: adicionada route service provider para pessoa

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -97,6 +97,10 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('api')
             ->prefix('api/fornecedor')
             ->group(base_path('routes/api/fornecedor.php'));
+
+            Route::middleware('api')
+            ->prefix('api/pessoa')
+            ->group(base_path('routes/api/pessoa.php'));
         });
     }
 }
