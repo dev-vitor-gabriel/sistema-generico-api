4f9739a3360a0eb413c44c8414316271547bc4e1
Lucas Barreto <lucas_barreto29@outlook.com>
2025-01-29 20:32:56 -0300

    feat: added index php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
-// api/index.php
-phpinfo();
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT);
+
+throw new RuntimeException('Hello Tracy!');
