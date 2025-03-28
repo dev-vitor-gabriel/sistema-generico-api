f7fc8972c68bb2de7b79eb97741410a321a02abd
“Vitor <dev.vitor.gabriel@gmail.com>
2025-03-28 00:57:53 -0300

    test: debugando

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-swagger-url', function () {
+    return url(config('l5-swagger.paths.docs_json'));
+});
