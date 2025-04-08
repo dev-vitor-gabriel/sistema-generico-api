f4508d3c0172977dbfba38a9d6100a0407f88150
“Vitor <dev.vitor.gabriel@gmail.com>
2025-04-08 00:08:01 -0300

    feat: criando route web para swagger;

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -17,7 +17,7 @@ return [
             ],
 
             'paths' => [
-                'docs_json' => 'api-docs.json',  // ou o caminho correto se for outro
+                'docs_json' => 'api/docs/api-docs.json',
                 'docs_yaml' => 'api-docs.yaml',  // se for o caso
                 'assets' => 'docs/assets',   // Mude de 'vendor/l5-swagger/' para o caminho correto
                 'swagger_ui' => '/docs',
