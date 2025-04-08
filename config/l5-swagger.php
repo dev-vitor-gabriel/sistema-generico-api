e459c9b273227354b527c1b78057ad8a0b5b9b66
“Vitor <dev.vitor.gabriel@gmail.com>
2025-04-08 01:18:43 -0300

    fix: tentando arrumar swagger

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -55,6 +55,12 @@ return [
                 ],
             ],
         ],
+        'servers' => [
+            [
+                'url' => env('L5_SWAGGER_SERVER_URL', 'http://localhost:8000/api'),
+                'description' => 'API Server',
+            ],
+        ],
     ],
     'defaults' => [
         'routes' => [
