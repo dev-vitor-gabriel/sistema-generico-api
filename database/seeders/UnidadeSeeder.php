7c44449c171513bce0279e1749ac5607bf6f9436
Lucas Barreto <lucas_barreto29@outlook.com>
2025-04-12 10:59:11 -0300

    feat: updated seeder

--- database/seeders/UnidadeSeeder.php
+++ database/seeders/UnidadeSeeder.php
@@ -13,21 +13,21 @@ class UnidadeSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('tb_unidade')->insert([
+        DB::table('tb_unidade')->updateOrInsert([
             'des_unidade_und'  => 'Unidade',
             'des_reduz_unidade_und'  => 'UN',
             'created_at' => now(),
             'updated_at' => now(),
             'id_centro_custo_und'  => 1,
         ]);
-        DB::table('tb_unidade')->insert([
+        DB::table('tb_unidade')->updateOrInsert([
             'des_unidade_und'  => 'Quilo',
             'des_reduz_unidade_und'  => 'Kg',
             'created_at' => now(),
             'updated_at' => now(),
             'id_centro_custo_und'  => 2,
         ]);
-        DB::table('tb_unidade')->insert([
+        DB::table('tb_unidade')->updateOrInsert([
             'des_unidade_und'  => 'Litros',
             'des_reduz_unidade_und'  => 'L',
             'created_at' => now(),
