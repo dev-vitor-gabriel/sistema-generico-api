623cf09d60be01c4c0df5ef4e62a7301ebc8a8e8
Jhammes <dean.morais@hotmail.com>
2025-04-16 02:18:09 -0300

    feat: criando repository para o cliente

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -6,6 +6,8 @@ use App\Interfaces\CargoRepositoryInterface;
 use App\Repositories\CargoRepository;
 use App\Interfaces\CentroCustoRepositoryInterface;
 use App\Repositories\CentroCustoRepository;
+use App\Interfaces\ClienteRepositoryInterface;
+use App\Repositories\ClienteRepository;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -18,6 +20,7 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         $this->app->bind(CargoRepositoryInterface::class, CargoRepository::class);
         $this->app->bind(CentroCustoRepositoryInterface::class, CentroCustoRepository::class);
+        $this->app->bind(ClienteRepositoryInterface::class, ClienteRepository::class);
     }
 
     /**
---
+++ app/Repositories/ClienteRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\ClienteRepositoryInterface;
+use App\Models\Cliente;
+
+class ClienteRepository implements ClienteRepositoryInterface
+{
+    public function create($request, $id_empresa)
+    {
+        $result = Cliente::create($request,$id_empresa);
+
+        return $result;
+    }
+
+    public function getAll($id_empresa, $filter, $per_page, $page_number)
+    {
+        $result = Cliente::getAll($id_empresa, $filter, $per_page, $page_number);
+
+        return $result;
+    }
+
+    public function getById($id_cliente, $id_empresa)
+    {
+        $result = Cliente::getById($id_cliente, $id_empresa);
+
+        return $result;
+    }
+
+    public function updateReg($id_empresa, $id_cliente, $request)
+    {
+        $result = Cliente::updateReg($id_empresa, $id_cliente, $request);
+
+        return $result;
+    }
+
+    public function deleteReg($id_empresa, $id_cliente)
+    {
+        $result = Cliente::deleteReg($id_empresa, $id_cliente);
+
+        return $result;
+    }
+}
