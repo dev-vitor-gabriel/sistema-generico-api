2e1fa6caa49aceeab1ed97bb724031143829f214
“Vitor <dev.vitor.gabriel@gmail.com>
2025-04-15 21:01:27 -0300

    fix: remove model Cargo;

--- app/Http/Controllers/API/CargoController.php
+++ app/Http/Controllers/API/CargoController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Interfaces\CargoRepositoryInterface;
-use App\Models\Cargo;
 use Illuminate\Http\Request;
 
 class CargoController extends Controller
