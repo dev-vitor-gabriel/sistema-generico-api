8344c506eb2000f41272fc065b91fcb3e1486f47
Jhammes <dean.morais@hotmail.com>
2025-04-17 11:49:02 -0300

    feat: correcao de variavel

--- app/Interfaces/InstituicaoPagamentoRepositoryInterface.php
+++ app/Interfaces/InstituicaoPagamentoRepositoryInterface.php
@@ -6,7 +6,7 @@ interface InstituicaoPagamentoRepositoryInterface
 {
     public function create($request, $id_empresa);
     public function getAll($id_empresa, $filter, $per_page, $page_number);
-    public function getById($id_empresa, $instituicao_pagamento);
-    public function updateReg($id_empresa, $instituicao_pagamento, $request);
-    public function deleteReg($id_empresa, $instituicao_pagamento);
+    public function getById($id_empresa, $id_instituicao_pagamento);
+    public function updateReg($id_empresa, $id_instituicao_pagamento, $request);
+    public function deleteReg($id_empresa, $id_instituicao_pagamento);
 }
