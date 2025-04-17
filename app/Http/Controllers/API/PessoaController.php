68fabbdc92ff17dfbb0ca5395c4c983e3f8d8575
Jhammes <dean.morais@hotmail.com>
2025-04-17 11:46:28 -0300

    fix: remove model pessoa

--- app/Http/Controllers/API/PessoaController.php
+++ app/Http/Controllers/API/PessoaController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Interfaces\PessoaRepositoryInterface;
-use App\Models\Pessoa;
 use Illuminate\Http\Request;
 use App\Helpers\ValidateString;
 
