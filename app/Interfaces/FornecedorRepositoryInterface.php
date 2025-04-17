f2cd1a4c5b56e705e1717f729cc1f852d47f15fe
Jhammes <dean.morais@hotmail.com>
2025-04-17 11:49:02 -0300

    feat: correcao de variavel

--- app/Interfaces/FornecedorRepositoryInterface.php
+++ app/Interfaces/FornecedorRepositoryInterface.php
@@ -6,7 +6,7 @@ interface FornecedorRepositoryInterface
 {
     public function create($request, $id_empresa);
     public function getAll($id_empresa, $filter, $per_page, $page_number);
-    public function getById($id_empresa, $fornecedor);
-    public function updateReg($id_empresa, $fornecedor, $request);
-    public function deleteReg($id_empresa, $fornecedor);
+    public function getById($id_empresa, $id_fornecedor);
+    public function updateReg($id_empresa, $id_fornecedor, $request);
+    public function deleteReg($id_empresa, $id_fornecedor);
 }
--- app/Interfaces/MaterialRepositoryInterface.php
+++ app/Interfaces/MaterialRepositoryInterface.php
@@ -6,7 +6,7 @@ interface MaterialRepositoryInterface
 {
     public function create($request, $id_empresa);
     public function getAll($id_empresa, $filter, $per_page, $page_number);
-    public function getById($id_empresa, $material);
-    public function updateReg($id_empresa, $material, $request);
-    public function deleteReg($id_empresa, $material);
+    public function getById($id_empresa, $id_material);
+    public function updateReg($id_empresa, $id_material, $request);
+    public function deleteReg($id_empresa, $id_material);
 }
